b33f134fc8d40991415480338e740544826ce8c0
kaien <kaienkira@gmail.com>
2017-02-13 16:49:33 +0800

    update build_release.sh

--- build_release.sh
+++ build_release.sh
@@ -6,12 +6,15 @@ arch_list="amd64 386"
 
 for os in $os_list
 do
+    if [ "$os" = "windows" ]
+    then
+        ext=.exe
+    else
+        ext=
+    fi
+
     for arch in $arch_list
     do
-        if [ "$os" = "windows" ]
-        then
-            ext=.exe
-        fi
         GOOS=$os GOARCH=$arch go build \
             -o bin/twilight-line-go-server-$os-$arch$ext $proj_path/server
         GOOS=$os GOARCH=$arch go build \
