2b61996c8987264aefa68f43ace2aaf1c2c90a10
kaien <kaienkira@gmail.com>
2019-07-09 14:37:54 +0800

    update build scripts

---
+++ build_linux.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+proj_path=.
+os=linux
+arch=amd64
+
+GOOS=$os GOARCH=$arch go build \
+    -o bin/twilight-line-go-server-$os-$arch $proj_path/server
+if [ $? -ne 0 ]; then exit 1; fi
+
+GOOS=$os GOARCH=$arch go build \
+    -o bin/twilight-line-go-client-$os-$arch $proj_path/client
+if [ $? -ne 0 ]; then exit 1; fi
+
+exit 0
