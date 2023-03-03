83109913dc395c87b6a25a658b75884ccfd08ee6
kaien <kaienkira@gmail.com>
2023-03-03 18:36:51 +0800

    work sync

--- build_linux.sh
+++ build_linux.sh
@@ -5,13 +5,13 @@ arch=amd64
 
 cd client && \
 GOOS="$os" GOARCH="$arch" go build \
-    -o ../bin/twilight-line-go-client-"$os"-"$arch".exe && \
+    -o ../bin/twilight-line-go-client-"$os"-"$arch" && \
 cd - >/dev/null
 if [ $? -ne 0 ]; then exit 1; fi
 
 cd server && \
 GOOS="$os" GOARCH="$arch" go build \
-    -o ../bin/twilight-line-go-server-"$os"-"$arch".exe && \
+    -o ../bin/twilight-line-go-server-"$os"-"$arch" && \
 cd - >/dev/null
 if [ $? -ne 0 ]; then exit 1; fi
 
